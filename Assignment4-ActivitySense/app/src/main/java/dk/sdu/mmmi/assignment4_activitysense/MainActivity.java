21974d4a3847fe82dfb73d3a48c0c0c66464acaf
Steffen Hansen <stef.hansen@live.dk>
2017-10-24 16:08:14 +0200

    Added Assignment 3

---
+++ Assignment4-ActivitySense/app/src/main/java/dk/sdu/mmmi/assignment4_activitysense/MainActivity.java
@@ -0,0 +1,82 @@
+package dk.sdu.mmmi.assignment4_activitysense;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.ActivityRecognitionClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ActivityBroadcastListener {
+
+    private static final String TAG = "MainActivity";
+
+    private ActivityRecognitionClient client;
+    private PendingIntent pendingIntent;
+    GoogleApiClient apiClient;
+    private BroadcastReceiver activityReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        client = ActivityRecognition.getClient(this);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ActivityRecognizedService.BROADCAST_NAME);
+        activityReceiver = new ActivityReceiver(this);
+        registerReceiver(activityReceiver, intentFilter);
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addApi(ActivityRecognition.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        apiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        client.removeActivityUpdates(pendingIntent);
+        unregisterReceiver(activityReceiver);
+        apiClient.disconnect();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        Intent intent = new Intent(this, ActivityRecognizedService.class);
+        pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        client.requestActivityUpdates(3000, pendingIntent);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
+    }
+
+    @Override
+    public void gotNewActivity(Intent i) {
+        Log.d(TAG, "Got broadcast!");
+    }
+}
